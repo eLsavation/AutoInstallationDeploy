83566bd29e5f00cca238bf5bcc6e9d3ca9f67d26
eLsavation <51361409+eLsavation@users.noreply.github.com>
2023-09-06 11:46:50 +0700

    Update install-golang.sh

--- install-golang.sh
+++ install-golang.sh
@@ -1,5 +1,5 @@
 curl -OL https://go.dev/dl/go1.18.1.linux-amd64.tar.gz
-tar -C /usr/local -xvf go1.18.1.linux-amd64.tar.gz
+sudo tar -C /usr/local -xvf go1.18.1.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 source ~/.profile
 go version
