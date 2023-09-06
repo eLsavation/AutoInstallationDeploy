8774ae1c9a75e9a5dabe075e7eab36f4b548d4cb
eLsavation <51361409+eLsavation@users.noreply.github.com>
2023-09-06 12:44:27 +0700

    Update create-db.sh

--- create-db.sh
+++ create-db.sh
@@ -2,24 +2,24 @@
 
 db_name=
 db_user=
-db_password=
+db_password=""
 
 # Make sure that NOBODY can access the server without a password
-mysql -e "UPDATE mysql.user SET Password = PASSWORD('CHANGEME') WHERE User = 'root'"
+sudo mysql -e "UPDATE mysql.user SET Password = PASSWORD('CHANGEME') WHERE User = 'root'"
 # Kill the anonymous users
-mysql -e "DROP USER ''@'localhost'"
+sudo mysql -e "DROP USER ''@'localhost'"
 # Because our hostname varies we'll use some Bash magic here.
-mysql -e "DROP USER ''@'$(hostname)'"
+sudo mysql -e "DROP USER ''@'$(hostname)'"
 # Kill off the demo database
-mysql -e "DROP DATABASE test"
+sudo mysql -e "DROP DATABASE test"
 # Create new database
-mysql -e "CREATE DATABASE $db_name"
+sudo mysql -e "CREATE DATABASE $db_name"
 # Create new user
-mysql -e "CREATE USER '$db_user'@'%' IDENTIFIED BY '$db_password'"
+sudo mysql -e "CREATE USER '$db_user'@'%' IDENTIFIED BY '$db_password'"
 # Grant Access to DB
-mysql -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'localhost'"
+sudo mysql -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'localhost'"
 # Make our changes take effect
-mysql -e "FLUSH PRIVILEGES"
+sudo mysql -e "FLUSH PRIVILEGES"
 # Any subsequent tries to run queries this way will get access denied because lack of usr/pwd param
 
 
