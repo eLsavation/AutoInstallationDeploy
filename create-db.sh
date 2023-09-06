1d241abea6db0c14cfc959841ca2cc55e5295573
eLsavation <51361409+eLsavation@users.noreply.github.com>
2023-09-06 13:01:54 +0700

    Update create-db.sh

--- create-db.sh
+++ create-db.sh
@@ -5,14 +5,11 @@ db_user=""
 db_password=""
 #
 # Create new database
-sudo mysql -e "CREATE DATABASE $db_name"
+sudo mysql -e "CREATE DATABASE if not exists $db_name"
 # Create new user
-sudo mysql -e "CREATE USER '$db_user'@'localhost' IDENTIFIED BY '$db_password'"
+sudo mysql -e "CREATE USER if not exists '$db_user'@'localhost' IDENTIFIED BY '$db_password'"
 # Grant Access to DB
 sudo mysql -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'localhost'"
 # Make our changes take effect
 sudo mysql -e "FLUSH PRIVILEGES"
-# Any subsequent tries to run queries this way will get access denied because lack of usr/pwd param
-
-
-
+#
