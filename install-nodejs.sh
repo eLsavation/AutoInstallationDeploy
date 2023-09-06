083bcbb92ad4be7c34ebeb0035c554deb390b7c6
eLsavation <51361409+eLsavation@users.noreply.github.com>
2023-09-06 11:37:26 +0700

    Update install-nodejs.sh

--- install-nodejs.sh
+++ install-nodejs.sh
@@ -1,9 +1,12 @@
 sudo apt install npm -y
 sudo apt install curl -y
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
-source ~/.bashrc
-sudo cp ~/.bashrc /home/devops
+sudo cp ~/.bashrc /home/devops || true
 cd /home/devops
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+source ~/.bashrc
 nvm ls-remote
 nvm install 18
 nvm use 18
