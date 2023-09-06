10c65df76c2f0a950de0ad63a7057005b9398e91
eLsavation <51361409+eLsavation@users.noreply.github.com>
2023-09-06 12:12:02 +0700

    Update php-automate.sh

--- php-automate.sh
+++ php-automate.sh
@@ -12,21 +12,15 @@ Cyan='\033[0;36m'         # Cyan
 #
 echo -n "$Cyan \n Installing PHP.. $Color_Off"
 echo -n "$Cyan \n Installing PHP in Progress \n $Color_Off"
-sudo apt-get install software-properties-common -y > /dev/null 2>&1
-sudo add-apt-repository ppa:ondrej/php --yes > /dev/null 2>&1
-echo -n "$Green \n Repository PHP added \n $Color_Off"
-echo -n "$Green \n Updating System... $Color_Off"
-sudo apt-get update -y > /dev/null 2>&1
-echo -n "$Green \n System Updated. \n $Color_Off"
-sudo apt-get install php8.1 libapache2-mod-php8.1 libapache2-mod-fcgid php8.1-cli php8.1-curl php8.1-gd php8.1-gmp php8.1-mbstring php8.1-mysql php8.1-snmp php8.1-xml php8.1-zip -y > /dev/null 2>&1
-echo -n "$Green \n PHP Installed $Color_Off"
-sudo systemctl start php8.1-fpm > /dev/null 2>&1
-echo -n "$Green \n PHP Started $Color_Off"
-sudo systemctl enable php8.1-fpm > /dev/null 2>&1
-echo -n "$Green \n PHP Enabled \n $Color_Off"
-sudo a2enmod actions fcgid alias proxy_fcgi > /dev/null 2>&1
-echo -n "$Green \n a2enmod Proxy Enabled \n $Color_Off"
-sudo systemctl restart apache2 > /dev/null 2>&1
-echo -n "$Green \n Apache2 has restarted \n $Color_Off"
-echo -n "$Cyan \n Installing PHP Success \n $Color_Off"
-#
+sudo apt update > /dev/null 2>&1
+sudo apt install --no-install-recommends php8.1 > /dev/null 2>&1
+sudo apt-get install php8.1-cli php8.1-curl php8.1-gd php8.1-gmp php8.1-mbstring php8.1-mysql php8.1-snmp php8.1-xml php8.1-zip -y > /dev/null 2>&1
+php -m
+php -v
+#install-compser
+curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
+HASH=`curl -sS https://composer.github.io/installer.sig`
+echo $HASH
+php -r "if (hash_file('SHA384', '/tmp/composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
+composer
