34bb0f07403b1df64acd90adcbb865b8bc35e432
eLsavation <51361409+eLsavation@users.noreply.github.com>
2023-11-22 15:26:25 +0700

    Update install-nginx.sh

--- install-nginx.sh
+++ install-nginx.sh
@@ -3,3 +3,5 @@ sudo systemctl start nginx
 sudo systemctl enable nginx
 sudo usermod -aG devops www-data
 sudo usermod -aG www-data devops
+sudo service nginx restart
+sudo service php8.1-fpm restart
