01c165235477f5ba6b5817f6a0774ca49c82b11d
eLsavation <51361409+eLsavation@users.noreply.github.com>
2024-01-23 14:19:05 +0700

    Update install-zabbixagent.sh

--- install-zabbixagent.sh
+++ install-zabbixagent.sh
@@ -2,5 +2,7 @@ sudo wget https://repo.zabbix.com/zabbix/6.4/ubuntu/pool/main/z/zabbix-release/z
 sudo dpkg -i zabbix-release_6.4-1+ubuntu$(lsb_release -rs)_all.deb
 sudo apt update
 sudo apt -y install zabbix-agent
-sudo systemctl restart zabbix-server zabbix-agent 
-sudo systemctl enable zabbix-server zabbix-agent
+sudo systemctl restart zabbix-agent 
+sudo systemctl enable zabbix-agent
+echo "ServerActive=139.59.105.236" | sudo tee -a /etc/zabbix/zabbix_agentd.conf
+echo "Hostname=$(hostname)" | sudo tee -a /etc/zabbix/zabbix_agentd.conf
